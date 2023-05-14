a254c9bfcbc28287f1996cbb4cbe2f98df5420b6
Gabriel Paniago <gmpaniago@hotmail.com>
2023-05-14 13:56:36 -0300

    Adição do Arquivo de Erros

---
+++ Anotações Gerais.c
@@ -0,0 +1,11 @@
+Sintaxe:
+	
+	Sintaxe são as regras da linguagem de programação, que devem ser seguidas para que o programa funcione corretamente, como por exemplo a utilização de ';'
+	no fechamento das funções, ou chamada de bibliotecas utilizando '#include <>'.
+	
+Semantica:
+
+
+Lista de Erros:
+	
+	
